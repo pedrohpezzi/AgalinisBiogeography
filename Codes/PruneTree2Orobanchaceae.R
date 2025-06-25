56c95c35fd333edbb374c230bf3d591cf28dc3a7
Pedro Henrique Pezzi <pedrohenriquepezzi@gmail.com>
2025-06-25 11:36:01 -0500

    Update PruneTree2Orobanchaceae.R

--- Codes/PruneTree2Orobanchaceae.R
+++ Codes/PruneTree2Orobanchaceae.R
@@ -6,12 +6,12 @@ library("ggtree")
 library("readr")
 
 #http://blog.phytools.org/2014/11/pruning-trees-to-one-member-per-genus.html
-# Script used to prune the Lamiales tree from Fonseca et al. (2021) down to only Orobanchaceae species.  
+# Script used to prune the Lamiales tree from Fonseca (2021) down to only Orobanchaceae species.  
 # The file `OrobanchaceaeSpList.txt` is in the `Data/congruification` folder.
 
 setwd("C:/Users/pedro/OneDrive/Desktop/Agalinis/Analyses/Congruification")
 
-tree <- read.tree("Fonseca2021_SuppMaterial/1-s2.0-S1055790321002207-mmc1/Lamiales_dated.nwk.tre")
+tree <- read.tree("Fonseca2021_SuppMaterial/1-s2.0-S1055790321002207-mmc1/Lamiales_dated.nwk.tre") #this can be download from the supplemental material of Fonseca (2021)
 ggtree(tree)
 ggtree(tree, layout='circular')
 
